917191f38215cc267c6fa5195101586e47eddbf3
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-09-17 23:01:21 +0900

    Add relationships between item and item type

--- app/Models/Item.js
+++ app/Models/Item.js
@@ -6,6 +6,10 @@ class Item extends Model {
   lists() {
     return this.belongsToMany('App/Models/List')
   }
+
+  type() {
+    return this.belongsTo('App/Models/ItemType', 'type_id', 'item_types.id')
+  }
 }
 
 module.exports = Item
--- app/Models/ItemType.js
+++ app/Models/ItemType.js
@@ -3,6 +3,9 @@
 const Model = use('Model')
 
 class ItemType extends Model {
+  items() {
+    return this.hasMany('App/Models/Item')
+  }
 }
 
 module.exports = ItemType
